393f453d25d241014eaf0e9a60d71b3d104904f7
maddymakesgames <madelinemakesgames@gmail.com>
2023-02-24 09:54:52 -0500

    Update triangle demo to use bind groups and keyboard input

--- render_test/shaders/triangle.wgsl
+++ render_test/shaders/triangle.wgsl
@@ -9,21 +9,24 @@ struct VertexOutput {
 };
 
 
-struct RoationUniform {
+struct ObjectUniform {
     rotation: vec4<f32>,
+    offset: vec2<f32>,
+    scale: f32,
 };
 
 @group(0)
 @binding(0)
-var<uniform> rotation: RoationUniform;
+var<uniform> state: ObjectUniform;
 
 @vertex
 fn vs_main(input: VertexInput) -> VertexOutput {
     var out: VertexOutput;
     let v = vec3(input.pos, 1.0);
-    let q = rotation.rotation;
+    let q = state.rotation;
     let tmp = cross(q.xyz, v) + q.w * v;
-    out.pos = vec4(v + 2.0 * cross(q.xyz, tmp), 1.0);
+    let pos = v + 2.0 * cross(q.xyz, tmp);
+    out.pos = vec4((pos + vec3(state.offset, 0.0)) * state.scale, 1.0);
     out.color = input.color;
     return out;
 }
--- render_test/src/main.rs
+++ render_test/src/main.rs
@@ -1,10 +1,12 @@
+use std::f32::consts::*;
+
 use bytemuck::{Pod, Zeroable};
-use math::{Mat4, Quat, Vec3};
+use math::{Quat, Vec2, Vec3};
 use render_lib::{
     manager::{RenderManager, SurfaceError},
     render_pipeline::{FrontFace, PrimitiveTopology},
     texture::FRAMEBUFFER,
-    wgpu::{Color, ShaderStages},
+    wgpu::{Color, PolygonMode, ShaderStages},
     Vertex,
 };
 use winit::{
@@ -13,17 +15,50 @@ use winit::{
     window::Window,
 };
 
+const FRAC_2_PI_3: f32 = FRAC_PI_3 * 2.0;
+
 #[derive(Clone, Copy, Pod, Zeroable, Vertex)]
 #[repr(C)]
 struct ColorPosVertex {
-    pos: [f32; 2],
+    pos: Vec2,
     color: [f32; 3],
 }
 
-#[derive(Clone, Copy, Pod, Zeroable)]
-#[repr(C)]
-struct RotationUniform {
+#[derive(Clone, Copy, Pod, Zeroable, Default)]
+#[repr(C, align(8))]
+struct TriangleUniform {
     rotation: Quat,
+    offset: Vec2,
+    scale: f32,
+    __padding: f32,
+}
+
+fn build_triangle_vertecies(
+    center: Vec2,
+    side_length: f32,
+    initial_rotation: f32,
+) -> [ColorPosVertex; 3] {
+    let a_theta = initial_rotation;
+    let b_theta = initial_rotation + FRAC_2_PI_3;
+    let c_theta = initial_rotation - FRAC_2_PI_3;
+    let a = center + side_length * Vec2::new(a_theta.cos(), a_theta.sin());
+    let b = center + side_length * Vec2::new(b_theta.cos(), b_theta.sin());
+    let c = center + side_length * Vec2::new(c_theta.cos(), c_theta.sin());
+
+    [
+        ColorPosVertex {
+            pos: a,
+            color: [1.0, 0.0, 0.0],
+        },
+        ColorPosVertex {
+            pos: b,
+            color: [0.0, 1.0, 0.0],
+        },
+        ColorPosVertex {
+            pos: c,
+            color: [0.0, 0.0, 1.0],
+        },
+    ]
 }
 
 fn main() {
@@ -34,30 +69,17 @@ fn main() {
     let vertex_buffer = manager
         .buffer_builder::<ColorPosVertex>(Some("Main Vertex Buffer"))
         .vertex()
-        .build_init(vec![
-            ColorPosVertex {
-                pos: [0.0, 0.5],
-                color: [1.0, 0.0, 0.0],
-            },
-            ColorPosVertex {
-                pos: [-0.5, -0.5],
-                color: [0.0, 1.0, 0.0],
-            },
-            ColorPosVertex {
-                pos: [0.5, -0.5],
-                color: [0.0, 0.0, 1.0],
-            },
-        ]);
+        .build_init(build_triangle_vertecies(Vec2::ZERO, 1.0, 0.0).to_vec());
 
     let uniform_buffer = manager
-        .buffer_builder::<RotationUniform>(Some("Rotation Buffer"))
+        .buffer_builder::<TriangleUniform>(Some("Rotation Buffer"))
         .uniform()
         .copy_dst()
-        .build(std::mem::size_of::<RotationUniform>() as u64);
+        .build(1);
 
     let bind_group = manager
         .bind_group_builder(Some("Triangle Bind Group"))
-        .bind_uniform_buffer::<RotationUniform>(0, ShaderStages::VERTEX, uniform_buffer)
+        .bind_uniform_buffer::<TriangleUniform>(0, ShaderStages::VERTEX, uniform_buffer)
         .build();
 
     let triangle_shader = manager.register_shader(include_str!("../shaders/triangle.wgsl"), None);
@@ -66,6 +88,7 @@ fn main() {
         .vertex_shader(triangle_shader, "vs_main")
         .fragment_shader(triangle_shader, "fs_main")
         .topology(PrimitiveTopology::TriangleList)
+        .polygon_mode(PolygonMode::Fill)
         .front_face(FrontFace::Cw)
         .add_vertex_buffer(vertex_buffer)
         .add_bind_group(bind_group)
@@ -77,7 +100,8 @@ fn main() {
         .add_attachment(FRAMEBUFFER, Some(Color::BLACK), true)
         .build();
 
-    let mut time = 0.0;
+    let mut spinning = true;
+    let mut state = TriangleUniform::default();
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
@@ -96,6 +120,54 @@ fn main() {
                             },
                         ..
                     } => *control_flow = ControlFlow::Exit,
+                    WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Return),
+                                ..
+                            },
+                        ..
+                    } => {
+                        spinning = !spinning;
+                    }
+                    WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                virtual_keycode: Some(keycode),
+                                ..
+                            },
+                        ..
+                    } => match keycode {
+                        VirtualKeyCode::Left => {
+                            *state.offset.x_mut() -= 0.05;
+                        }
+                        VirtualKeyCode::Right => {
+                            *state.offset.x_mut() += 0.05;
+                        }
+                        VirtualKeyCode::Up => {
+                            *state.offset.y_mut() += 0.05;
+                        }
+                        VirtualKeyCode::Down => {
+                            *state.offset.y_mut() -= 0.05;
+                        }
+                        VirtualKeyCode::Space => {
+                            state.scale += 0.05;
+                        }
+                        VirtualKeyCode::LShift => {
+                            state.scale -= 0.05;
+                        }
+                        VirtualKeyCode::D =>
+                            if !spinning {
+                                state.rotation *= Quat::from_axis_angle(Vec3::Z, FRAC_PI_8 / 8.0);
+                            },
+                        VirtualKeyCode::A =>
+                            if !spinning {
+                                state.rotation *=
+                                    Quat::from_axis_angle(Vec3::Z, -(FRAC_PI_8 / 8.0));
+                            },
+                        _ => {}
+                    },
                     WindowEvent::Resized(size) => manager.resize(*size),
                     WindowEvent::ScaleFactorChanged {
                         new_inner_size: size,
@@ -106,10 +178,12 @@ fn main() {
             },
         Event::RedrawRequested(window_id) =>
             if window_id == manager.window.id() {
-                manager.write_to_buffer(uniform_buffer, &[RotationUniform {
-                    rotation: Quat::from_axis_angle(Vec3::Z, time),
-                }]);
-                time += 0.01;
+                if spinning {
+                    state.rotation *= Quat::from_axis_angle(Vec3::Z, FRAC_PI_8 / 24.0);
+                }
+                manager.write_to_buffer(uniform_buffer, &[state]);
+
+
                 match manager.render() {
                     Ok(_) => {}
                     Err(SurfaceError::OutOfMemory) | Err(SurfaceError::Lost) => manager.recreate(),
